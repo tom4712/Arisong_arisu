148c687799a52d83761a79c04d96e9ace2ba8f7f
SO819 <ndehknife@naver.com>
2017-09-22 20:46:28 +0900

    스플레시

---
+++ app/src/main/java/com/chkk/arisong_arisu/LodingAvtivity.java
@@ -0,0 +1,26 @@
+package com.chkk.arisong_arisu;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LodingAvtivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_loding_avtivity);
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+    Intent intent = new Intent(getBaseContext(),MainActivity.class);
+
+                startActivity(intent);
+                finish();
+            }
+        },2000);
+    }
+}
